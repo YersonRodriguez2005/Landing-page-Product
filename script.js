4d844965641f90b055ad7ead54b8654a0dc7ce76
Yerson Rodriguez <rodriguezyerson2005@gmail.com>
2024-09-05 14:42:39 -0500

    update 2 landing

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const images = [
-    'img1.webp',
-    'img2.webp',
-    'img3.webp',
-    'img4.webp',
-    'img5.webp'
+    '/assets/img1.webp',
+    '/assets/img2.webp',
+    '/assets/img3.webp',
+    '/assets/img4.webp',
+    '/assets/img5.webp'
 ];
 
 let currentIndex = 0;
